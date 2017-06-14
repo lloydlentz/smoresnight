100b97a630d6456d17dc789345a25a4f80db338d
Lloyd Lentz <cledwyn@gmail.com>
2017-06-14 10:46:55 -0500

    error trap the only serverside logic that exists

--- signup.php
+++ signup.php
@@ -8,37 +8,46 @@
 		use Twilio\Rest\Client;
 
 	if ($_GET["phone"]){
+		try {
+			$number = filter_var($_GET["phone"], FILTER_SANITIZE_NUMBER_INT);
+			//print($number);
 
-		$number = filter_var($_GET["phone"], FILTER_SANITIZE_NUMBER_INT);
-		//print($number);
+			$sql = "Insert into smoresfolks(number) values ('$number')";
 
-		$sql = "Insert into smoresfolks(number) values ('$number')";
+			if ($db->query($sql) === TRUE) {
+				// echo $sql;
+			} else {
+				echo "Error: " . $sql . "<br>" . $db->error;
+			}
 
-		if ($db->query($sql) === TRUE) {
-			// echo $sql;
-		} else {
-			echo "Error: " . $sql . "<br>" . $db->error;
+
+
+			$twilioclient = new Client($twiliosid, $twiliotoken);
+			// Use the client to do fun stuff like send text messages!
+			$twilioclient->messages->create(
+				// the number you'd like to send the message to
+				'+1' . $number,
+				array(
+					// A Twilio phone number you purchased at twilio.com/console
+					'from' => '+13213334488 ',
+					// the body of the text message you'd like to send
+					'body' => "Thanks for signing up. I'll shoot you a note when its smores night.!\n -Lloyd and team. ",
+					// 'mediaUrl' => "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
+					'mediaUrl' => "http://isitsmoresnight.com/cat.jpg",
+				)
+			);
+			
+
+			header("Location: thanks.jpg"); /* Redirect browser */
+			exit();
+		} catch (Exception $e) {
+			echo "<h1>YOU ERRORED MY INTERNETS</h1><img src=404.jpg>";
+
+			
 		}
 
-
-
-		$twilioclient = new Client($twiliosid, $twiliotoken);
-		// Use the client to do fun stuff like send text messages!
-		$twilioclient->messages->create(
-			// the number you'd like to send the message to
-			'+1' . $number,
-			array(
-				// A Twilio phone number you purchased at twilio.com/console
-				'from' => '+13213334488 ',
-				// the body of the text message you'd like to send
-				'body' => "Thanks for signing up. I'll shoot you a note when its smores night.!\n -Lloyd and team. ",
-				// 'mediaUrl' => "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg",
-				'mediaUrl' => "http://isitsmoresnight.com/cat.jpg",
-			)
-		);
+	} else {
+		echo "not sure how you got here.  try that again."
 	}
 
-
-	header("Location: thanks.jpg"); /* Redirect browser */
-	exit();
 ?>
