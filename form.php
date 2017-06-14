b992015ea27c8acfd9978fc35704c00bd58e3736
Lloyd Lentz <cledwyn@gmail.com>
2017-06-14 10:14:02 -0500

    tidy tidy

--- form.php
+++ form.php
@@ -4,164 +4,147 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Signup</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400' rel='stylesheet' type='text/css'>
 
+	<style>
+		h1, input::-webkit-input-placeholder, button {
+			font-family: 'roboto', sans-serif;
+			-webkit-transition: all 0.3s ease-in-out;
+			transition: all 0.3s ease-in-out;
+		}
 
-	<head>
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+		h1 {
+			height: 100px;
+			width: 100%;
+			font-size: 18px;
+			background: #18aa8d;
+			color: white;
+			line-height: 150%;
+			border-radius: 3px 3px 0 0;
+			box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.2);
+		}
 
-  
-      <style>
-      h1, input::-webkit-input-placeholder, button {
-  font-family: 'roboto', sans-serif;
-  -webkit-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-}
+		form {
+			box-sizing: border-box;
+			width: 260px;
+			margin: 100px auto 0;
+			box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
+			padding-bottom: 40px;
+			border-radius: 3px;
+		}
+		form h1 {
+			box-sizing: border-box;
+			padding: 20px;
+		}
 
-h1 {
-  height: 100px;
-  width: 100%;
-  font-size: 18px;
-  background: #18aa8d;
-  color: white;
-  line-height: 150%;
-  border-radius: 3px 3px 0 0;
-  box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.2);
-}
+		input {
+			margin: 40px 25px;
+			width: 200px;
+			display: block;
+			border: none;
+			padding: 10px 0;
+			border-bottom: solid 1px #1abc9c;
+			-webkit-transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
+			transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
+			background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) 96%, #1abc9c 4%);
+			background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 96%, #1abc9c 4%);
+			background-position: -200px 0;
+			background-size: 200px 100%;
+			background-repeat: no-repeat;
+			color: #0e6252;
+		}
+		input:focus, input:valid {
+			box-shadow: none;
+			outline: none;
+			background-position: 0 0;
+		}
+		input:focus::-webkit-input-placeholder, input:valid::-webkit-input-placeholder {
+			color: #1abc9c;
+			font-size: 11px;
+			-webkit-transform: translateY(-20px);
+							transform: translateY(-20px);
+			visibility: visible !important;
+		}
 
-form {
-  box-sizing: border-box;
-  width: 260px;
-  margin: 100px auto 0;
-  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-  padding-bottom: 40px;
-  border-radius: 3px;
-}
-form h1 {
-  box-sizing: border-box;
-  padding: 20px;
-}
+		button {
+			border: none;
+			background: #1abc9c;
+			cursor: pointer;
+			border-radius: 3px;
+			padding: 6px;
+			width: 200px;
+			color: white;
+			margin-left: 25px;
+			box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2);
+		}
+		button:hover {
+			-webkit-transform: translateY(-3px);
+							transform: translateY(-3px);
+			box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.2);
+		}
 
-input {
-  margin: 40px 25px;
-  width: 200px;
-  display: block;
-  border: none;
-  padding: 10px 0;
-  border-bottom: solid 1px #1abc9c;
-  -webkit-transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
-  transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
-  background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) 96%, #1abc9c 4%);
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 96%, #1abc9c 4%);
-  background-position: -200px 0;
-  background-size: 200px 100%;
-  background-repeat: no-repeat;
-  color: #0e6252;
-}
-input:focus, input:valid {
-  box-shadow: none;
-  outline: none;
-  background-position: 0 0;
-}
-input:focus::-webkit-input-placeholder, input:valid::-webkit-input-placeholder {
-  color: #1abc9c;
-  font-size: 11px;
-  -webkit-transform: translateY(-20px);
-          transform: translateY(-20px);
-  visibility: visible !important;
-}
+		.follow {
+			width: 42px;
+			height: 42px;
+			border-radius: 50px;
+			background: #03A9F4;
+			display: inline-block;
+			margin: 50px calc(50% - 21px);
+			white-space: nowrap;
+			padding: 13px;
+			box-sizing: border-box;
+			color: white;
+			-webkit-transition: all 0.2s ease;
+			transition: all 0.2s ease;
+			font-family: Roboto, sans-serif;
+			text-decoration: none;
+			box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.2);
+		}
+		.follow i {
+			margin-right: 20px;
+			-webkit-transition: margin-right 0.2s ease;
+			transition: margin-right 0.2s ease;
+		}
+		.follow:hover {
+			width: 134px;
+			-webkit-transform: translateX(-50px);
+							transform: translateX(-50px);
+		}
+		.follow:hover i {
+			margin-right: 10px;
+		}
 
-button {
-  border: none;
-  background: #1abc9c;
-  cursor: pointer;
-  border-radius: 3px;
-  padding: 6px;
-  width: 200px;
-  color: white;
-  margin-left: 25px;
-  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2);
-}
-button:hover {
-  -webkit-transform: translateY(-3px);
-          transform: translateY(-3px);
-  box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.2);
-}
+		:root .ad
+		{ display: none !important; }
+	</style>
 
-.follow {
-  width: 42px;
-  height: 42px;
-  border-radius: 50px;
-  background: #03A9F4;
-  display: inline-block;
-  margin: 50px calc(50% - 21px);
-  white-space: nowrap;
-  padding: 13px;
-  box-sizing: border-box;
-  color: white;
-  -webkit-transition: all 0.2s ease;
-  transition: all 0.2s ease;
-  font-family: Roboto, sans-serif;
-  text-decoration: none;
-  box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.2);
-}
-.follow i {
-  margin-right: 20px;
-  -webkit-transition: margin-right 0.2s ease;
-  transition: margin-right 0.2s ease;
-}
-.follow:hover {
-  width: 134px;
-  -webkit-transform: translateX(-50px);
-          transform: translateX(-50px);
-}
-.follow:hover i {
-  margin-right: 10px;
-}
+	<script>
+		window.console = window.console || function(t) {};
 
-    </style>
+		if (document.location.search.match(/type=embed/gi)) {
+			window.parent.postMessage("resize", "*");
+		}
 
-  <script>
-  window.console = window.console || function(t) {};
-</script><style type="text/css">
-:root .ad
-{ display: none !important; }</style>
+		function telephoneCheck(str) {
+			var a = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(str);
+		
+			var go = document.getElementById('go');
+			go.disabled = !a;
+			go.innerHTML  = a ? "submit" : "enter a valid phone";
+			return a;
+			//alert(a);
+		}
+		//telephoneCheck("(555) 555-5555");
+	</script>
 
-  
-  
-  <script>
-  if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage("resize", "*");
-  }
-
-	
-
-	function telephoneCheck(str) {
-		var a = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(str);
-	
-		var go = document.getElementById('go');
-		go.disabled = !a;
-		go.innerHTML  = a ? "submit" : "enter a valid phone";
-		return a;
-		//alert(a);
-	}
-	//telephoneCheck("(555) 555-5555");
-</script>
-
-</head>
 </head>
 <body>
-
-<form action="../signup.php">
-  <h1>Enter your cell, and I'll shoot you a text when it's smores night.</h1>
-  <input name="phone" placeholder="Cell Number" type="phone" required="" onchange="console.log(telephoneCheck(this.value)); " onkeyup="console.log(telephoneCheck(this.value)); ">
-  <button id="go">Submit</button>
-</form>
-
-
+	<form action="signup.php">
+		<h1>Enter your cell, and I'll shoot you a text when it's smores night.</h1>
+		<input name="phone" placeholder="Cell Number" type="phone" required="" onchange="console.log(telephoneCheck(this.value));" onkeyup="console.log(telephoneCheck(this.value)); ">
+		<button id="go">Submit</button>
+	</form>
 </body>
 </html> 
